d11b4027a5a0697e0967461f190f84cf4d1b5665
dalinger1023 <wanghuiling1023@sina.com>
2019-12-15 16:39:07 +0800

    在userjs中封装了logout方法,并调用

--- web_back/tools/user.js
+++ web_back/tools/user.js
@@ -1,5 +1,6 @@
 //使用user.js对项目中与[用户操作相关的请求]
 var user = {
+    //添加[登录功能]
     login: function (options) {
         $.ajax({
             type: 'post',
@@ -16,5 +17,21 @@ var user = {
                 }
             }
         })
-    }
+    },
+
+    //添加[退出功能]
+    logout: function (options) {
+        $.ajax({
+            type: 'post',
+            url: LOGOUT,
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
+        })
+    },
+
 }
