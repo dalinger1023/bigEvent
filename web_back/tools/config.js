f06f73c05418cc59971d2965bc26e4e45099a86d
dalinger1023 <wanghuiling1023@sina.com>
2019-12-15 15:06:54 +0800

    用config管理接口地址

---
+++ web_back/tools/config.js
@@ -0,0 +1,10 @@
+//js中用来保存网站中的一些统一的配置信息
+//  -如所有的接口地址
+//  -为避免这些信息的变量名和其他代码中的变量名冲突
+//  -可以将变量名书写为全大写的格式
+//  -命名的多个部分使用_连接
+
+var BASE_URL = 'http://localhost:8000/';
+
+var LOGIN = BASE_URL + 'admin/login';
+var LOGOUT = BASE_URL + 'admin/logout';
