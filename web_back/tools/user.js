03571f1608d69cc87e87da19d1e999ad117f4be2
dalinger1023 <wanghuiling1023@sina.com>
2019-12-15 16:03:27 +0800

    user.js 参数封装为对象

--- web_back/tools/user.js
+++ web_back/tools/user.js
@@ -1,14 +1,20 @@
 //使用user.js对项目中与[用户操作相关的请求]
 var user = {
-    login: function (data, success) {
+    login: function (options) {
         $.ajax({
             type: 'post',
             url: LOGIN,
             data: {
-                user_name: data.user_name,
-                password: data.password
+                user_name: options.data.user_name,
+                password: options.data.password
             },
-            success: success
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
         })
     }
 }
