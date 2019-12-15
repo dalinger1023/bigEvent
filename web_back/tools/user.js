e74d69426b84480319f3d503f8e20c31511d2aec
dalinger1023 <wanghuiling1023@sina.com>
2019-12-15 15:42:05 +0800

    新增user.js 统一管理用户操作相关
    如ajax请求

---
+++ web_back/tools/user.js
@@ -0,0 +1,14 @@
+//使用user.js对项目中与[用户操作相关的请求]
+var user = {
+    login: function (data, success) {
+        $.ajax({
+            type: 'post',
+            url: LOGIN,
+            data: {
+                user_name: data.user_name,
+                password: data.password
+            },
+            success: success
+        })
+    }
+}
